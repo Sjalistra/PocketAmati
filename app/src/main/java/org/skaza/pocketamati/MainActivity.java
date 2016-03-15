a8a10dc8bfccdb90256bdc4abc524be4575a1961
sjal <sjalistra@yahoo.no>
2016-03-15 21:56:12 +0100

    It works!

---
+++ app/src/main/java/org/skaza/pocketamati/MainActivity.java
@@ -0,0 +1,27 @@
+package org.skaza.pocketamati;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    /*
+        TODO: Kalkulator wymiarów
+        TODO: Gotowe wymiary małych instrumentów
+        TODO: Instrukcje z ilustracjami wykonania elementów i Receptury lakierów
+        TODO: Galeria dokumentacji
+        TODO: Notatki
+
+        TODO: Cechy stylistyczne najwazniejszych twórców, bio
+        TODO: Emisja herzów do strojenia skrzypków i Mini-pianinko jedna oktawa
+        TODO: Książka adresowa: dla np. dostawców drewna, innych lutników itp.
+        TODO: Wishlista narzedzi
+        TODO: Galeria drogich modeli skrzypiec
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
