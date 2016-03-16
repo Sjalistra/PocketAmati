b1d36f50af5fcc93a34c11e93c87cf71224e032d
sjalistra <sjalistra@yahoo.no>
2016-03-16 03:21:34 +0100

    Karol

---
+++ app/src/main/java/org/skaza/pocketamati/ExpensiveActivity.java
@@ -0,0 +1,13 @@
+package org.skaza.pocketamati;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class ExpensiveActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expensive);
+    }
+}
